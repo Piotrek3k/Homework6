b91a6fcd924ccc9ad1116c49e5a4af68e57ecc5a
Piotr Pośpiech <piootrp01@gmail.com>
2024-05-07 20:13:46 +0200

    fix task6 challenge

--- Task6.js
+++ Task6.js
@@ -12,8 +12,8 @@ const curry = (func,arity) => {
         }
     }
 }
-const placeholder = Symbol("_")
-const curry2 = (func,arity) => {
+const placeholder = "_"
+const curry2 = (func, arity = func.length) => {
     // second function to perform currying to allow partial application. Function also implements a placeholder symbol for missing arguments
     return function curriedFunction(...curriedArguments) {
         if (arity === curriedArguments.length && !curriedArguments.includes(placeholder)) { // in this code function also checks if the placeholder has been changed to a value before returning the provided function
@@ -21,16 +21,17 @@ const curry2 = (func,arity) => {
         }
         else {
             return function(...newArguments) { 
-                // mapping function to replace placeholders with values
                 let newCurriedArguments = curriedArguments.map(curriedArgument => {
-                    if(curriedArgument === placeholder) {
-                        if(newArguments){
+                    if(curriedArgument === placeholder ) { 
+                        if(newArguments.length > 0) {
+
                             return curriedArgument = newArguments.shift() // shifting from new arguments         
                         }
                     }
                     else {
                         return curriedArgument  // if the curriedArgument is not a placeholder return it
                     }
+                    
                 })
                 return curriedFunction(...newArguments, ...newCurriedArguments)    // recursively the newCurriedFunction is executed and new provided argument is added to the array-like object of arguments
             }
@@ -42,22 +43,32 @@ const curry2 = (func,arity) => {
 function multiply(a, b, c) {
     return a * b * c;
 }
+//console.log(multiply.length)
+// // const curriedMultiply = curry(multiply, 3);
 
-// const curriedMultiply = curry(multiply, 3);
+// // const step1 = curriedMultiply(2); // Returns a curried function
+// // const step2 = step1(3)(4); // Returns a curried function
+// // //const result = step2(4); // Returns the final result: 2 * 3 * 4 = 24
 
-// const step1 = curriedMultiply(2); // Returns a curried function
-// const step2 = step1(3)(4); // Returns a curried function
-// //const result = step2(4); // Returns the final result: 2 * 3 * 4 = 24
-
-// console.log("Result:", step2); // Expected: 24
+// // console.log("Result:", step2); // Expected: 24
 
 const curried2Multiply = curry2(multiply, 3);
 //console.log(curried2Multiply)
-const step1 = curried2Multiply(5)(placeholder)(6)
-const result = step1(2)
+const step11 = curried2Multiply(5)(placeholder)(6)
+const result = step11(2)
 console.log(result)
-//console.log(step1)
+// console.log(step1)
 // const step2 = step1(3)
 // //console.log(step2)
 // const finalStep = step2(2)
 // console.log(finalStep)
+
+const extendCurryMultiply = curry2(multiply)
+
+const step1 = extendCurryMultiply("_")
+const step2 = step1("_")
+let addMissingArgs = step2(4)
+let step3 = addMissingArgs(2)
+let result2 = step3(3)
+
+console.log(result2)
